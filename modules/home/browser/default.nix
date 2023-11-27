44468d525cb313c4f6530e6212af60df615301e0
twoneis <sanjay29@sapura.de>
2023-11-27 20:07:25 +0100

    added librewolf

--- modules/home/browser/default.nix
+++ modules/home/browser/default.nix
@@ -2,4 +2,12 @@
   programs.firefox = {
     enable = true;
   };
+
+  programs.librewolf = {
+    enable = true;
+    settings = {
+      "webgl.disabled" = false;
+      "privacy.resistFingerprinting" = false;
+    };
+  };
 }
