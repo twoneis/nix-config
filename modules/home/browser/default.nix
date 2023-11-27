7664de17faecf141186753777fc3c7414161152d
twoneis <sanjay29@sapura.de>
2023-11-27 20:14:47 +0100

    removed librewolf

--- modules/home/browser/default.nix
+++ modules/home/browser/default.nix
@@ -2,12 +2,4 @@
   programs.firefox = {
     enable = true;
   };
-
-  programs.librewolf = {
-    enable = true;
-    settings = {
-      "webgl.disabled" = false;
-      "privacy.resistFingerprinting" = false;
-    };
-  };
 }
