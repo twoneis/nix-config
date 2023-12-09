ae94a2beca5e861122cf4a98119d2e71ae684c7f
twoneis <sanjay29@sapura.de>
2023-12-09 12:11:36 +0100

    removed r

--- modules/home/default.nix
+++ modules/home/default.nix
@@ -11,7 +11,6 @@
     ./git
     ./media
     ./notes
-    ./r
     ./terminal
     ./utils
   ];
--- modules/home/r/default.nix
+++
@@ -1,8 +0,0 @@
-{ pkgs, ... }: {
-  home.packages = with pkgs; [
-    R
-    rstudioWrapper
-    pandoc
-    texlive.combined.scheme-full
-  ];
-}
