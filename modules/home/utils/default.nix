66d555d3c45da748996d54ee15f7b31ca37dcefc
twoneis <sanjay29@sapura.de>
2024-01-06 23:30:02 +0100

    hyfetch

--- modules/home/utils/default.nix
+++ modules/home/utils/default.nix
@@ -3,9 +3,30 @@
     man-pages
     man-pages-posix
     gnome.nautilus
+    tree
   ];
 
   programs.bottom = {
     enable = true;
+  };
+
+  programs.hyfetch = {
+    enable = true;
+    settings = {
+      preset = "transgender";
+      mode = "rgb";
+      light_dark = "dark";
+      lightness = 0.65;
+      color_align = {
+          mode = "horizontal";
+          custom_colors = [];
+          fore_back = null;
+      };
+      backend = "neofetch";
+      args = null;
+      distro = "nixos_old";
+      pride_month_shown = [];
+      pride_month_disable = false;
+    };
   };
 }
