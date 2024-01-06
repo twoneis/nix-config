262fba83ba0e2ee1ab8b8e967607bee9dd13b263
twoneis <sanjay29@sapura.de>
2024-01-06 16:14:34 +0100

    change to gh package to avoid overwriting configuration

--- modules/home/git/default.nix
+++ modules/home/git/default.nix
@@ -1,11 +1,9 @@
-{ ... }: {
+{ pkgs, ... }: {
   programs.git = {
     enable = true;
     userName = "twoneis";
     userEmail = "sanjay29@sapura.de";
   };
 
-  programs.gh = {
-    enable = true;
-  };
+  home.packages = with pkgs; [ gh ];
 }
