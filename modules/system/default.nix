04b498f466eb7571736cec17abac1d72c7274a72
twoneis <sanjay29@sapura.de>
2024-01-07 14:55:41 +0100

    changed gc time (old was invalid?)

--- modules/system/default.nix
+++ modules/system/default.nix
@@ -21,7 +21,7 @@
     gc = {
       automatic = true;
       dates = "weekly";
-      options = "--delete-older-than 7d";
+      options = "--delete-older-than 30d";
     };
     settings.auto-optimise-store = true;
   };
