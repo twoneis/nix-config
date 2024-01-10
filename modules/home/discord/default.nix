cdde3603416ac9a1f9f21b247aa1ebf83961d5fe
twoneis <sanjay29@sapura.de>
2024-01-10 14:57:27 +0100

    switch back to simple webcord

--- modules/home/discord/default.nix
+++ modules/home/discord/default.nix
@@ -1,14 +1,5 @@
 { pkgs, ... }: {
   home.packages = with pkgs; [
-    dorion
+    webcord
   ];
-
-  home.file = {
-    ".config/dorion/config.json" = {
-      source = ./config.json;
-    };
-    ".config/dorion/themes/rose-pine.css" = {
-      source = ./rose-pine.css;
-    };
-  };
 }
