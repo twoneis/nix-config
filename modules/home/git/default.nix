4594a4067c1e12a7f98174b74406b568133ee8c0
twoneis <sanjay29@sapura.de>
2024-01-12 14:03:05 +0100

    added more git config

--- modules/home/git/default.nix
+++ modules/home/git/default.nix
@@ -3,6 +3,10 @@
     enable = true;
     userName = "twoneis";
     userEmail = "sanjay29@sapura.de";
+    extraConfig = {
+      init.defaultBranch = "main";
+      push.autoSetupRemote = true;
+    };
   };
 
   home.packages = with pkgs; [ gh ];
