693ad9f7c9b5b38344143dec35f5df039a13302f
twoneis <sanjay29@sapura.de>
2024-01-16 03:56:25 +0100

    moved output to file

--- flake.nix
+++ flake.nix
@@ -29,40 +29,5 @@
     };
   };
 
-  outputs = { nixpkgs, home-manager, nixos-hardware, nur, niri, ... }: {
-    nixosConfigurations = let
-      commonModules = [
-          ./modules/system
-
-          niri.nixosModules.default
-
-          home-manager.nixosModules.home-manager {
-            nixpkgs.overlays = [
-              nur.overlay
-            ];
-
-            home-manager = {
-              useGlobalPkgs = true;
-              useUserPackages = true;
-              users.twoneis = import ./modules/home;
-            };
-          }
-      ];
-    in {
-      desktop = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-        modules = [
-          ./devices/desktop
-        ] ++ commonModules;
-      };
-
-      surface = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-        modules = [
-          ./devices/surface
-          nixos-hardware.nixosModules.microsoft-surface-pro-intel
-        ] ++ commonModules;
-      };
-    };
-  };
+  outputs = inputs: import ./outputs.nix inputs;
 }
