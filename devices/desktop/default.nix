3097ba2b05b600183659c74079edea9569b8ca16
twoneis <sanjay29@sapura.de>
2024-01-20 18:08:29 +0100

    added bluetooth config

--- devices/desktop/default.nix
+++ devices/desktop/default.nix
@@ -40,6 +40,13 @@
     package = config.boot.kernelPackages.nvidiaPackages.vulkan_beta;
   };
 
+  hardware.bluetooth = {
+    enable = true;
+    powerOnBoot = true;
+  };
+
+  services.blueman.enable = true;
+
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave
@@ -47,5 +54,4 @@
   # Before changing this value read the documentation for this option
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "23.05"; # Did you read the comment?
-
 }
--- modules/home/discord/default.nix
+++ modules/home/discord/default.nix
@@ -1,6 +1,7 @@
 { pkgs, ... }: {
   home.packages = with pkgs; [
     dorion
+    webcord
   ];
 
   home.file = {
