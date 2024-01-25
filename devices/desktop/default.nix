6f8e981659539a90fddfc4dfc1c940fac752b729
twoneis <sanjay29@sapura.de>
2024-01-25 17:48:36 +0100

    more bluetooth config

--- devices/desktop/default.nix
+++ devices/desktop/default.nix
@@ -43,6 +43,11 @@
   hardware.bluetooth = {
     enable = true;
     powerOnBoot = true;
+    settings = {
+      General = {
+        Experimental = true;
+      };
+    };
   };
 
   services.blueman.enable = true;
