17997a05231269d418fc084e50247603f0cdca18
twoneis <sanjay29@sapura.de>
2024-01-31 21:51:06 +0100

    added mopidy init

--- modules/home/media/default.nix
+++ modules/home/media/default.nix
@@ -2,7 +2,18 @@
   home.packages = with pkgs; [
     spotify
     image-roll
+    mopidy-muse
   ];
+
+  services.mopidy = {
+    enable = true;
+    extensionPackages = with pkgs; [
+      mopidy-local
+      mopidy-tidal
+      mopidy-spotify
+      mopidy-tidal
+    ];
+  };
 
   programs.mpv = {
     enable = true;
