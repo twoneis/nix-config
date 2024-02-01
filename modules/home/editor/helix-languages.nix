45a4582ff720b2a089ca3b043b545650133d76e5
twoneis <sanjay29@sapura.de>
2024-02-01 02:59:49 +0100

    removed not working bqn configuration

--- modules/home/editor/helix-languages.nix
+++ modules/home/editor/helix-languages.nix
@@ -3,16 +3,7 @@
     asm-lsp = {
       command = "asm-lsp";
     };
-    bqnlsp = {
-      command = "bqnlsp";
-    };
   };
-
-  grammar = [{
-    name = "bqn";
-    source.git = "https://github.com/shnarazk/tree-sitter-bqn";
-    source.rev = "f0e2f74ba75c6bf0a143a1d3870317c9c8064af9";
-  }];
 
   language = [{
     name = "gas";
@@ -22,16 +13,5 @@
     indent = { tab-width = 8; unit = " "; };
     roots = [];
     file-types = ["S"];
-  } {
-    name = "bqn";
-    file-types = [ "bqn" ];
-    comment-token = "#";
-    indent = { tab-width = 4; unit = " "; };
-    shebangs = [ "bqn" "cbqn" ];
-    roots = [];
-    injection-regex = "bqn";
-    scope = "scope.bqn";
-    language-servers = [ "bqnlsp" ];
-    language-id = "bqn";
   }];
 }
