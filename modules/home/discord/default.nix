a845e9171109f6c5a18f43be38b7ca3ec0d516bc
twoneis <sanjay29@sapura.de>
2024-02-09 22:54:15 +0100

    removed dorion

--- modules/home/discord/default.nix
+++ modules/home/discord/default.nix
@@ -1,15 +1,5 @@
 { pkgs, ... }: {
   home.packages = with pkgs; [
-    dorion
     webcord
   ];
-
-  home.file = {
-    ".config/dorion/config.json" = {
-      source = ./config.json;
-    };
-    ".config/dorion/themes/rose-pine.css" = {
-      source = ./rose-pine.css;
-    };
-  };
-}
+m
