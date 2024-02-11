ed8de34c0f350ea3aad1d5214c698a414c0b9e7f
twoneis <sanjay29@sapura.de>
2024-02-11 23:13:43 +0100

    updated rose pine theme for alacritty

--- modules/home/terminal/alacritty-config.nix
+++ modules/home/terminal/alacritty-config.nix
@@ -2,44 +2,28 @@
   # Rose Pine
   colors = {
     primary = {
-      background = "0x191724";
-      foreground = "0xe0def4";
+      foreground = "#e0def4";
+      background = "#191724";
+      dim_foreground = "#908caa";
+      bright_foreground = "#e0def4";
     };
     cursor = {
-      text = "0xe0def4";
-      cursor = "0x524f67";
+      text = "#e0def4";
+      cursor = "#524f67";
     };
     vi_mode_cursor = {
-      text = "0xe0def4";
-      cursor = "0x524f67";
+      text = "#e0def4";
+      cursor = "#524f67";
     };
-    line_indicator = {
-      foreground = "None";
-      background = "None";
-    };
-    selection = {
-      text = "0xe0def4";
-      background = "0x403d52";
-    };
-    normal = {
-      black = "0x26233a";
-      red = "0xeb6f92";
-      green = "0x31748f";
-      yellow = "0xf6c177";
-      blue = "0x9ccfd8";
-      magenta = "0xc4a7e7";
-      cyan = "0xebbcba";
-      white = "0xe0def4";
-    };
-    bright = {
-      black = "0x6e6a86";
-      red = "0xeb6f92";
-      green = "0x31748f";
-      yellow = "0xf6c177";
-      blue = "0x9ccfd8";
-      magenta = "0xc4a7e7";
-      cyan = "0xebbcba";
-      white = "0xe0def4";
+    search = {
+      matches = {
+        foreground = "#908caa";
+        background = "#26233a";
+      };
+      focused_match = {
+        foreground = "#191724";
+        background = "#ebbcba";
+      };
     };
     hints = {
       start = {
@@ -51,7 +35,103 @@
         background = "#1f1d2e";
       };
     };
+    line_indicator = {
+      foreground = "None";
+      background = "None";
+    };
+    footer_bar = {
+      foreground = "#e0def4";
+      background = "#1f1d2e";
+    };
+    selection = {
+      text = "#e0def4";
+      background = "#1f1d2e";
+    };
+    normal = {
+      black = "#26233a";
+      red = "#eb6f92";
+      green = "#31748f";
+      yellow = "#f6c177";
+      blue = "#9ccfd8";
+      magenta = "#c4a7e7";
+      cyan = "#ebbcba";
+      white = "#e0def4";
+    };
+    bright = {
+      black = "#6e6a86";
+      red = "#eb6f92";
+      green = "#31748f";
+      yellow = "#f6c177";
+      blue = "#9ccfd8";
+      magenta = "#c4a7e7";
+      cyan = "#ebbcba";
+      white = "#e0def4";
+    };
+    dim = {
+      black = "#6e6a86";
+      red = "#eb6f92";
+      green = "#31748f";
+      yellow = "#f6c177";
+      blue = "#9ccfd8";
+      magenta = "#c4a7e7";
+      cyan = "#ebbcba";
+      white = "#e0def4";
+    };
   };
+
+  # Rose Pine (old)
+  # colors = {
+  #   primary = {
+  #     background = "0x191724";
+  #     foreground = "0xe0def4";
+  #   };
+  #   cursor = {
+  #     text = "0xe0def4";
+  #     cursor = "0x524f67";
+  #   };
+  #   vi_mode_cursor = {
+  #     text = "0xe0def4";
+  #     cursor = "0x524f67";
+  #   };
+  #   line_indicator = {
+  #     foreground = "None";
+  #     background = "None";
+  #   };
+  #   selection = {
+  #     text = "0xe0def4";
+  #     background = "0x403d52";
+  #   };
+  #   normal = {
+  #     black = "0x26233a";
+  #     red = "0xeb6f92";
+  #     green = "0x31748f";
+  #     yellow = "0xf6c177";
+  #     blue = "0x9ccfd8";
+  #     magenta = "0xc4a7e7";
+  #     cyan = "0xebbcba";
+  #     white = "0xe0def4";
+  #   };
+  #   bright = {
+  #     black = "0x6e6a86";
+  #     red = "0xeb6f92";
+  #     green = "0x31748f";
+  #     yellow = "0xf6c177";
+  #     blue = "0x9ccfd8";
+  #     magenta = "0xc4a7e7";
+  #     cyan = "0xebbcba";
+  #     white = "0xe0def4";
+  #   };
+  #   hints = {
+  #     start = {
+  #       foreground = "#908caa";
+  #       background = "#1f1d2e";
+  #     };
+  #     end = {
+  #       foreground = "#6e6a86";
+  #       background = "#1f1d2e";
+  #     };
+  #   };
+  # };
 
   # # Rose Pine Dawn
   # colors = {
