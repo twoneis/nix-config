ea056ddce222b720e666e46214f275155f2014bc
twoneis <sanjay29@sapura.de>
2024-02-11 03:41:53 +0100

    upsie plaintext password

--- modules/server/access-point.nix
+++ modules/server/access-point.nix
@@ -5,7 +5,7 @@
       INTERNET_IFACE = "enp2s0";
       WIFI_IFACE = "wlp1s0";
       SSID = "home";
-      PASSPHRASE = builtins.readFile /home/twoneis/ap-passkey;
+      PASSPHRASE = "FCKask4net";
     };
   };
 }
