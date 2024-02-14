4c6b4c66463087db723120ab009af164d70fc8c4
twoneis <sanjay29@sapura.de>
2024-02-14 23:25:06 +0100

    changed to dorion (config not working yet)

--- modules/system/home/discord/default.nix
+++ modules/system/home/discord/default.nix
@@ -1,5 +1,14 @@
 { pkgs, ... }: {
   home.packages = with pkgs; [
-    webcord
+    dorion
   ];
+
+  home.file = {
+    # ".config/dorion/config.json" = {
+    #   source = ./dorion-config.json;
+    # };
+    # ".config/dorion/themes/rose-pine.css" = {
+    #   source = ./rose-pine.theme.css;
+    # };
+  };
 }
