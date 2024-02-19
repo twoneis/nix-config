aad22df4648e1492b1138cb353a1a555396c0b7a
twoneis <sanjay29@sapura.de>
2024-02-19 16:25:00 +0100

    switched dorion for vesktop

--- modules/system/home/discord/default.nix
+++ modules/system/home/discord/default.nix
@@ -1,14 +1,15 @@
 { pkgs, ... }: {
   home.packages = with pkgs; [
-    dorion
+    vesktop
+    # dorion
   ];
 
-  home.file = {
-    ".config/dorion/config.json" = {
-      source = ./dorion-config.json;
-    };
-    ".config/dorion/themes/rose-pine.theme.css" = {
-      source = ./rose-pine.theme.css;
-    };
-  };
+  # home.file = {
+  #   ".config/dorion/config.json" = {
+  #     source = ./dorion-config.json;
+  #   };
+  #   ".config/dorion/themes/rose-pine.theme.css" = {
+  #     source = ./rose-pine.theme.css;
+  #   };
+  # };
 }
