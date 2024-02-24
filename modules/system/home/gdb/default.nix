67495214a560d28e6237f79e42b63bc309cfc3d3
twoneis <sanjay29@sapura.de>
2024-02-24 14:51:13 +0100

    less gdb options

--- modules/system/home/gdb/default.nix
+++ modules/system/home/gdb/default.nix
@@ -1,12 +1,7 @@
 { ... }: {
   home.file = {
-    ".gef.rc" = {
-      # source = ./gef-for-c;
-      source = ./gef-for-asm;
-    };
     ".gdbinit" = {
-      # source = ./gdb-for-c;
-      source = ./gdb-for-asm;
+      source = ./gdbinit;
     };
   };
 }
