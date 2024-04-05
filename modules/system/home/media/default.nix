41dc6a44cabca3361e51b64b9f2eb59441061ab3
twoneis <sanjay29@sapura.de>
2024-04-05 03:31:17 +0200

    removed unused service

--- modules/system/home/media/default.nix
+++ modules/system/home/media/default.nix
@@ -2,18 +2,7 @@
   home.packages = with pkgs; [
     spotify
     image-roll
-    mopidy-muse
   ];
-
-  services.mopidy = {
-    enable = true;
-    extensionPackages = with pkgs; [
-      mopidy-local
-      mopidy-tidal
-      mopidy-spotify
-      mopidy-tidal
-    ];
-  };
 
   programs.mpv = {
     enable = true;
