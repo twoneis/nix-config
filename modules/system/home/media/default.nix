f4e4918fbee1c229036cbffa6813dddbd07495c5
twoneis <sanjay29@sapura.de>
2024-04-26 00:46:16 +0200

    changed spotifyd quality

--- modules/system/home/media/default.nix
+++ modules/system/home/media/default.nix
@@ -12,7 +12,7 @@
     settings = {
       global = {
         device_name = osConfig.networking.hostName;
-        bitrate = 160;
+        bitrate = 320;
         volume_normalisation = true;
         autoplay = false;
         zeroconf_port = 57621;
