d80c8b3385bf7274b2e577dd560090e85148b839
twoneis <sanjay29@sapura.de>
2024-05-07 00:11:51 +0200

    smaller hardware config

--- devices/ellaca/hardware-config.nix
+++ devices/ellaca/hardware-config.nix
@@ -1,8 +1,4 @@
-{ config, lib, modulesPath, ... }: {
-  imports = [ 
-    (modulesPath + "/installer/scan/not-detected.nix")
-  ];
-
+{ config, lib, ... }: {
   boot.initrd.availableKernelModules = [ "nvme" "xhci_pci" "ahci" "usb_storage" "usbhid" "sd_mod" ];
   boot.initrd.kernelModules = [ ];
   boot.kernelModules = [ "kvm-amd" ];
