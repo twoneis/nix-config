7fd7e0964a2c785b3abd3e7447de933ea42b1534
twoneis <sanjay29@sapura.de>
2024-05-07 23:46:02 +0200

    added grub device again

--- devices/ellaca/default.nix
+++ devices/ellaca/default.nix
@@ -16,6 +16,7 @@
       enable = true;
       efiSupport = true;
       efiInstallAsRemovable = true;
+      device = "nodev";
       useOSProber = true;
       font = null;
       splashImage = null;
