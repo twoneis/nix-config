75db472d2b4e413292ff3dd11ac0bea21c76588f
twoneis <sanjay29@sapura.de>
2024-05-07 23:44:32 +0200

    manual hardware config if that's the issue

--- devices/ellaca/hardware-config.nix
+++ devices/ellaca/hardware-config.nix
@@ -4,6 +4,33 @@
   boot.kernelModules = [ "kvm-amd" ];
   boot.extraModulePackages = [ ];
 
+  fileSystems = {
+    "/" = {
+      device = "/dev/disk/by-uuid/7d29fef5-9fdf-43b4-ad17-a94774f802b5";
+      fsType = "btrfs";
+      options = [ "subvol=rootfs" "compress=zstd" "noatime" ];
+    };
+    "/boot" = {
+      device = "/dev/disk/by-uuid/DCD8-29D7";
+      fsType = "vfat";
+    };
+    "/nix" = {
+      device = "/dev/disk/by-uuid/7d29fef5-9fdf-43b4-ad17-a94774f802b5";
+      fsType = "btrfs";
+      options = [ "subvol=nix" "compress=zstd" "noatime" ];
+    };
+    "/home" = {
+      device = "/dev/disk/by-uuid/7d29fef5-9fdf-43b4-ad17-a94774f802b5";
+      fsType = "btrfs";
+      options = [ "subvol=home" "compress=zstd" "noatime" ];
+    };
+    "/ext" = {
+      device = "/dev/disk/by-uuid/90aa2cba-41dc-4efb-b509-822d1e2bc525";
+      fsType = "btrfs";
+      options = [ "subvol=ext" "compress=zstd" "noatime" ];
+    };
+  };
+
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
   # still possible to use this option, but it's recommended to use it in conjunction
