7d76d70687654980df5df7e963317df39d899d95
twoneis <sanjay29@sapura.de>
2024-05-27 14:19:26 +0200

    proper nvidia config

--- modules/nvidia/default.nix
+++ modules/nvidia/default.nix
@@ -1,12 +1,9 @@
-{ config, ... }: {
+{ lib, config, ... }: lib.mkIf (config.withNvidia) {
   # Load nvidia driver
   services.xserver.videoDrivers = ["nvidia"];
 
   hardware.nvidia = {
     modesetting.enable = true;
-    powerManagement.enable = false;
-    powerManagement.finegrained = false;
-    open = false;
     nvidiaSettings = false;
     package = config.boot.kernelPackages.nvidiaPackages.vulkan_beta;
   };
