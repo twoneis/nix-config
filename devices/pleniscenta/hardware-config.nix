3aebfcd1b7af2fc27cad7fea8858b510e82cf90f
Sanjay Chacku Purakal <48321566+twoneis@users.noreply.github.com>
2024-06-02 18:10:00 +0200

    fixed disk uuids

--- devices/pleniscenta/hardware-config.nix
+++ devices/pleniscenta/hardware-config.nix
@@ -3,7 +3,7 @@
     initrd = {
       availableKernelModules = [ "ahci" "xhci_pci" "ums_realtek" "usbhid" "usb_storage" "sd_mod" "sdhci_pci" ];
       kernelModules = [ ];
-      luks.devices.cryptroot.device = "/dev/disk/by-uuid/XXXX-XXXX";
+      luks.devices.cryptroot.device = "/dev/disk/by-uuid/470a6d23-9f08-4c4d-afbb-63d6df1ab6a6";
     };
     kernelModules = [ "kvm-intel" ];
     extraModulePackages = [ ];
@@ -15,19 +15,22 @@
       fsType = "vfat";
     };
     "/" = {
-      device = "/dev/disk/by-uuid/";
+      device = "/dev/disk/by-uuid/75e6536a-4836-4f50-86b3-f9250edca3c0";
       fsType = "btrfs";
       options = [ "subvol=root" "compress=zstd" "noatime" ];
     };
     "/nix" = {
+      device = "/dev/disk/by-uuid/75e6536a-4836-4f50-86b3-f9250edca3c0";
       fsType = "btrfs";
       options = [ "subvol=nix" "compress=zstd" "noatime" ];
     };
     "/persist" = {
+      device = "/dev/disk/by-uuid/75e6536a-4836-4f50-86b3-f9250edca3c0";      
       fsType = "btrfs";
       options = [ "subvol=persist" "compress=zstd" "noatime" ];
-    };
+    }; 
     "/swap" = {
+      device = "/dev/disk/by-uuid/75e6536a-4836-4f50-86b3-f9250edca3c0";
       fsType = "btrfs";
       options = [ "subvol=swap" "noatime" ];
     };
