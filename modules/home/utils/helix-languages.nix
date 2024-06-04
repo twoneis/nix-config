c7a37c8d2a562840a5676353d68470cb3f427570
twoneis <sanjay29@sapura.de>
2024-06-04 22:46:42 +0200

    added .h files to c for formatting and lsp in helix

--- modules/home/utils/helix-languages.nix
+++ modules/home/utils/helix-languages.nix
@@ -12,20 +12,21 @@
     language-servers = [ "asm-lsp" ];
     indent = { tab-width = 8; unit = " "; };
     roots = [];
-    file-types = ["S"];
+    file-types = [ "S" ];
   } {
     name = "json";
     auto-format = true;
-    formatter = { command = "prettier"; args = ["--parser" "json"]; };
+    formatter = { command = "prettier"; args = [ "--parser" "json" ]; };
   } {
     name = "jsonc";
     auto-format = true;
     formatter.command = "prettier";
-    formatter.args = ["--parser" "jsonc"];
+    formatter.args = [ "--parser" "jsonc" ];
   } {
     name = "c";
     auto-format = true;
-    formatter = { command = "clang-format"; args = ["-style=file"]; };
+    formatter = { command = "clang-format"; args = [ "-style=file" ]; };
     indent = { tab-width = 8; unit = " "; };
+    file-types = [ "c" "h" ];
   }];
 }
