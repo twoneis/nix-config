d930c9be4b5e3eb0e426d43aff1d398afbc68743
twoneis <sanjay29@sapura.de>
2024-06-21 14:44:42 +0200

    renamed option

--- modules/gnome/default.nix
+++ modules/gnome/default.nix
@@ -1,4 +1,4 @@
 { pkgs, lib, config, ... }: lib.mkIf (config.withGnome) {
-  services.xserver.displayManager.sessionPackages = [ pkgs.gnome.gnome-session.sessions ];
+  services.displayManager.sessionPackages = [ pkgs.gnome.gnome-session.sessions ];
   environment.systemPackages = [ pkgs.gnome.gnome-shell pkgs.gnome-randr ];
 }
