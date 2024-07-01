e0f3c025fb3988e766ed55ceb87d671796ce3614
twoneis <sanjay29@sapura.de>
2024-07-01 21:26:46 +0200

    exclude packages from plasma

--- modules/plasma/default.nix
+++ modules/plasma/default.nix
@@ -1,3 +1,18 @@
-{ lib, config, ... }: lib.mkIf (config.withPlasma) {
+{ lib, config, pkgs, ... }: lib.mkIf (config.withPlasma) {
   services.desktopManager.plasma6.enable = true;
+  environment.plasma6.excludePackages = with pkgs; [
+    plasma-browser-integration
+    konsole
+    ark
+    elisa
+    gwenview
+    okular
+    kate
+    khelpcenter
+    print-manager
+    dolphin
+    dolphin-plugins
+    spectacle
+    ffmpegthumbs
+  ];
 }
