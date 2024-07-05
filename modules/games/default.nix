9e5b56293b155f450f9a9bc0cb93f2664e3d88d8
twoneis <sanjay29@sapura.de>
2024-07-06 00:59:58 +0200

    more minimal games

--- modules/games/default.nix
+++ modules/games/default.nix
@@ -1,11 +1,4 @@
-{ lib, config, pkgs, ... }: lib.mkIf config.withGames {
+{ lib, config, ... }: lib.mkIf config.withGames {
   programs.steam.enable = true;
-  programs.gamescope.enable = true;
   programs.gamemode.enable = true;
-
-  home-manager.users.twoneis = {
-    home.packages = with pkgs; [
-      lutris
-    ];
-  };
 }
