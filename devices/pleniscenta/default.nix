6ca492584d72d75eb1a0be3e8eaafe0bb32f722a
twoneis <sanjay29@sapura.de>
2024-07-21 02:11:12 +0200

    added disks for all devices

--- devices/pleniscenta/default.nix
+++ devices/pleniscenta/default.nix
@@ -1,6 +1,7 @@
-{ pkgs, ... }: {
+{ config, pkgs, ... }: {
   imports = [
     ./options.nix
+    ./disks.nix
   ];
 
   nixpkgs.hostPlatform = "x86_64-linux";
@@ -10,7 +11,7 @@
   boot = {
     initrd = {
       availableKernelModules = [ "ahci" "xhci_pci" "ums_realtek" "usbhid" "usb_storage" "sd_mod" "sdhci_pci" ];
-      luks.devices."root".device = "/dev/disk/by-uuid/470a6d23-9f08-4c4d-afbb-63d6df1ab6a6";
+      luks.devices."root".device = "/dev/disk/by-uuid/${config.disks.crypt}";
     };
 
     kernelModules = [ "kvm-intel" ];
@@ -26,26 +27,26 @@
 
   fileSystems = {
     "/boot" = {
-      device = "/dev/disk/by-uuid/4A39-D0DF";
+      device = "/dev/disk/by-uuid/${config.disks.boot}";
       fsType = "vfat";
     };
     "/" = {
-      device = "/dev/disk/by-uuid/75e6536a-4836-4f50-86b3-f9250edca3c0";
+      device = "/dev/disk/by-uuid/${config.disks.root}";
       fsType = "btrfs";
       options = [ "subvol=root" "compress=zstd" "noatime" ];
     };
     "/nix" = {
-      device = "/dev/disk/by-uuid/75e6536a-4836-4f50-86b3-f9250edca3c0";
+      device = "/dev/disk/by-uuid/${config.disks.root}";
       fsType = "btrfs";
       options = [ "subvol=nix" "compress=zstd" "noatime" ];
     };
     "/persist" = {
-      device = "/dev/disk/by-uuid/75e6536a-4836-4f50-86b3-f9250edca3c0";      
+      device = "/dev/disk/by-uuid/${config.disks.root}";
       fsType = "btrfs";
       options = [ "subvol=persist" "compress=zstd" "noatime" ];
     }; 
     "/swap" = {
-      device = "/dev/disk/by-uuid/75e6536a-4836-4f50-86b3-f9250edca3c0";
+      device = "/dev/disk/by-uuid/${config.disk.root}";
       fsType = "btrfs";
       options = [ "subvol=swap" "noatime" ];
     };
