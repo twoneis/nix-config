d2692a154a88c648fca95009502cea0f342d94b6
twoneis <sanjay29@sapura.de>
2024-10-13 17:05:13 +0200

    new theme for sddm

--- modules/niri/default.nix
+++ modules/niri/default.nix
@@ -18,16 +18,18 @@ in {
     };
 
     environment.systemPackages = [
-      (pkgs.where-is-my-sddm-theme.override ({
-        themeConfig = (import ./sddm.theme.nix { config = config; }).style;
-        variants = [ "qt5" ];
+      (pkgs.catppuccin-sddm.override ({
+        flavor = "mocha";
+        font = "Fira Code Nerd Font";
+        background = "${./sddm.wallpaper.png}";
+        loginBackground = true;
       }))
     ];
 
     services.displayManager.sddm = {
       enable = true;
       wayland.enable = true;
-      theme = "where_is_my_sddm_theme_qt5";
+      theme = "catppuccin-mocha";
     };
 
     programs.niri = {
