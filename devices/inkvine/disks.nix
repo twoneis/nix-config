5bad9ea0078d13c4f093cc8c302e70ca5df0d813
Sanjay Chacku Purakal <48321566+twoneis@users.noreply.github.com>
2024-10-13 13:55:29 +0000

    Update disks.nix

--- devices/inkvine/disks.nix
+++ devices/inkvine/disks.nix
@@ -1,7 +1,7 @@
 { ... }: {
   device.disks = {
-    boot = "5BB3-FDE6";
-    crypt = "8c2e23a2-301a-483d-9331-afc982082c8a";
-    root = "c08d5ede-433d-48cf-8344-17b20d32fb6c";
+    boot = "5BD3-FDE6";
+    crypt = "7171d4c2-fd27-45ed-b058-31bab14681e8";
+    root = "f2b17157-1e79-4a44-8349-e611c4768949";
   };
 }
