388bdd46b1d2443bc83f80ec3415f3302a294536
Sanjay Chacku Purakal <48321566+twoneis@users.noreply.github.com>
2024-10-13 13:07:37 +0000

    Update disks.nix

--- devices/inkvine/disks.nix
+++ devices/inkvine/disks.nix
@@ -1,7 +1,7 @@
 { ... }: {
   device.disks = {
-    boot = "D261-7199";
+    boot = "5BB3-FDE6";
     crypt = "8c2e23a2-301a-483d-9331-afc982082c8a";
-    root = "6ad1b73e-8fd7-4e03-850e-8eef3175036b";
+    root = "c08d5ede-433d-48cf-8344-17b20d32fb6c";
   };
 }
