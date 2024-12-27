435436d2ba3d2397c618bf030b7f38c97b9c7bd8
twoneis <sanjay29@sapura.de>
2024-12-27 11:40:51 +0100

    enable docker, disable vm

--- devices/inkvine/options.nix
+++ devices/inkvine/options.nix
@@ -3,8 +3,8 @@
     apps.enable = true;
     niri.enable = true;
     extraLayout.enable = true;
-    vm.enable = true;
     secureboot.enable = true;
+    containers.enable = true;
 
     stateVersion = "24.05";
     hmStateVersion = "24.11";
