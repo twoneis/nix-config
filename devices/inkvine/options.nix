d319ab0c5388f71f51f017b7cc8137dc9e71a1ce
twoneis <sanjay29@sapura.de>
2025-01-08 20:43:35 +0100

    enable secure boot

--- devices/inkvine/options.nix
+++ devices/inkvine/options.nix
@@ -3,7 +3,7 @@
     apps.enable = true;
     niri.enable = true;
     extraLayout.enable = true;
-    secureboot.enable = false;
+    secureboot.enable = true;
     containers.enable = true;
 
     stateVersion = "24.11";
