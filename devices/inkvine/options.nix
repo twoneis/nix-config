ce6cc745b7b876fa4b6e7c7af96acfa6fcd0c484
twoneis <sanjay29@sapura.de>
2025-01-08 20:29:46 +0100

    fix stuff

--- devices/inkvine/options.nix
+++ devices/inkvine/options.nix
@@ -1,10 +1,10 @@
 { ... }: {
   conf = {
-    apps.enable = false;
-    niri.enable = false;
+    apps.enable = true;
+    niri.enable = true;
     extraLayout.enable = true;
     secureboot.enable = false;
-    containers.enable = false;
+    containers.enable = true;
 
     stateVersion = "24.11";
     hmStateVersion = "24.11";
