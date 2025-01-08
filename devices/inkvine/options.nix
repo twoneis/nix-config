81b3f4d1cabe5c4f844d05719b544ce27a09ee5d
Mira Chacku Purakal <48321566+twoneis@users.noreply.github.com>
2025-01-08 19:00:43 +0100

    minimal to get started

--- devices/inkvine/options.nix
+++ devices/inkvine/options.nix
@@ -1,10 +1,10 @@
 { ... }: {
   conf = {
-    apps.enable = true;
-    niri.enable = true;
+    apps.enable = false;
+    niri.enable = false;
     extraLayout.enable = true;
     secureboot.enable = false;
-    containers.enable = true;
+    containers.enable = false;
 
     stateVersion = "24.11";
     hmStateVersion = "24.11";
