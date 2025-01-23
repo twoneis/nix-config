2d261827e0b3a573b9abacfdd03103f72684a59a
twoneis <sanjay29@sapura.de>
2025-01-23 18:59:08 +0100

    syntax

--- modules/apps/android.nix
+++ modules/apps/android.nix
@@ -1,5 +1,6 @@
-{ conf, lib }: let
+{ config, lib, ... }: let
   inherit (lib) mkIf;
+  inherit (config) conf;
 in mkIf conf.apps.enable {
   programs.adb.enable = true;
   users.users.${conf.username}.extraGroups = [ "adbusers" ];
