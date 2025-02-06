2415cd8d57a46e809b6c450098b76b1564162746
twoneis <sanjay29@sapura.de>
2025-02-06 18:20:31 +0100

    optional ssh

--- modules/ssh/default.nix
+++ modules/ssh/default.nix
@@ -1,7 +1,14 @@
-{ ... }: {
+{ lib, config, ... }: let
+  inherit (lib) mkIf;
+  inherit (config) conf;
+in mkIf conf.ssh.enable {
   services.openssh.enable = true;
 
   users.users.root.openssh.authorizedKeys.keys = [
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIB5i6DgsV5wbkkEJzFbN624Sz0CKg9LGplfJUjeCpAdE"
+  ];
+
+  users.users.${conf.username}.openssh.authorizedKeys.keys = [
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIB5i6DgsV5wbkkEJzFbN624Sz0CKg9LGplfJUjeCpAdE"
   ];
 }
