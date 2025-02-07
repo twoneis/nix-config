a09edd84780e72ac0fe7c83e6e681f50b3bc43dd
twoneis <sanjay29@sapura.de>
2025-02-07 20:20:02 +0100

    doesn't fail check at least

--- devices/ellaca/default.nix
+++ devices/ellaca/default.nix
@@ -7,7 +7,10 @@
 
   nixpkgs.hostPlatform = "x86_64-linux";
 
-  networking.hostName = "ellaca";
+  networking = {
+    hostName = "ellaca";
+    domain = "twoneis.site";
+  };
 
   boot = {
     initrd.availableKernelModules = [ "ata_piix" "uhci_hcd" "virtio_pci" "virtio_scsi" "sd_mod" ];
--- modules/fedi/default.nix
+++ modules/fedi/default.nix
@@ -1,9 +1,24 @@
 { lib, config, ... }: let
   inherit (lib) mkIf;
   inherit (config) conf;
+  name = "mira on fedi";
+  description = "miras akkoma instance";
+  email = "akkoma@chpu.eu";
 in mkIf conf.fedi.enable {
   services.akkoma = {
     enable = true;
+    config = {
+      ":pleroma" = {
+        ":instance" = {
+          name = name;
+          email = email;
+          description = description;
+        };
+        "Pleroma.Upload" = {
+          base_url = "https://media.fedi.twoneis.site/media";
+        };
+      };
+    };
     nginx = {
       serverName = "fedi.twoneis.site";
     };
