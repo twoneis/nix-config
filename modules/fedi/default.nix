6efa1dde3a90649a70af85068e88fff9ea1e72eb
twoneis <sanjay29@sapura.de>
2025-02-08 12:46:57 +0100

    last akkoma try

--- modules/fedi/default.nix
+++ modules/fedi/default.nix
@@ -2,36 +2,29 @@
   inherit (lib) mkIf;
   inherit (config) conf;
 in mkIf conf.fedi.enable {
-  services.postgresql.enable = true;
-  services.akkoma = {
-    enable = true;
-    initDb = {
-      enable = false;
-      password._secret = "/root/db_password";
-      username = "root";
-    };
-    config = {
-      ":pleroma" = {
-        ":instance" = {
-          name = "mira on fedi";
-          email = "akkoma@chpu.eu";
-          description = "miras fedi instance";
-          registration_opn = false;
-        };
-        "Pleroma.Upload" = {
-          base_url = "https://media.fedi.twoneis.site/media";
-        };
-        "Pleroma.Repo" = {
-          username = "akkoma";
-          password._secret = "/root/db_password";
-          database = "akkoma";
-          socket_dir = "/run/postgersql";
-          adapter = (pkgs.formats.elixirConf { }).lib.mkRaw "Ecto.Adapters.Postgres";
+  services = {
+    akkoma = {
+      enable = true;
+      user = "akkoma";
+      config = {
+        ":pleroma" = {
+          ":instance" = {
+            name = "mira on fedi";
+            description = "miras fedi instance";
+            email = "mira.cp.0909@gmail.com";
+            registration_open = true;
+          };
+
+          "Pleroma.Web.Endpoint" = {
+            url.host = "fedi.twoneis.site";
+          };
         };
       };
-    };
-    nginx = {
-      serverName = "fedi.twoneis.site";
+      nginx = {
+        serverName = "fedi.twoneis.site";
+        useACMEHost = "twoneis.site";
+        forceSSL = true;
+      };
     };
   };
 }
