1bf0ff35fc6cc7d3a185dd62ad6776fa398438a0
twoneis <sanjay29@sapura.de>
2025-02-08 16:02:55 +0100

    clear fedi module

--- modules/fedi/default.nix
+++ modules/fedi/default.nix
@@ -2,29 +2,4 @@
   inherit (lib) mkIf;
   inherit (config) conf;
 in mkIf conf.fedi.enable {
-  services = {
-    akkoma = {
-      enable = true;
-      user = "akkoma";
-      config = {
-        ":pleroma" = {
-          ":instance" = {
-            name = "mira on fedi";
-            description = "miras fedi instance";
-            email = "mira.cp.0909@gmail.com";
-            registration_open = true;
-          };
-
-          "Pleroma.Web.Endpoint" = {
-            url.host = "fedi.twoneis.site";
-          };
-        };
-      };
-      nginx = {
-        serverName = "fedi.twoneis.site";
-        useACMEHost = "twoneis.site";
-        forceSSL = true;
-      };
-    };
-  };
 }
